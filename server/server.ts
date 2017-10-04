09625fbb381a5cfe8a41cd59eb84327642e0d905
Pierre REPETTO-ANDIPATIN <pierre@anthillsolutions.ch>
2017-10-04 12:03:53 +0200

    fix: adapt files and jest

--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,22 @@
-import * as express from "express";
-import * as dotenv from "dotenv";
-import * as bodyParser from "body-parser";
+import * as express from 'express';
+import * as dotenv from 'dotenv';
+import * as bodyParser from 'body-parser';
 
-import * as homeController from "./controllers/home.controller";
+import * as homeController from './controllers/home.controller';
 
 dotenv.config();
 
 const app = express();
 
-app.set("port", process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.get("/", homeController.index);
+app.get('/', homeController.index);
 
-app.listen(app.get("port"), () => {
-  console.log(("  App is running at http://localhost:%d in %s mode"),
-    app.get("port"), app.get("env"));
-  console.log("  Press CTRL-C to stop\n");
+app.listen(app.get('port'), () => {
+  console.log(('App is running at http://localhost:%d in %s mode'),
+    app.get('port'), app.get('env'));
+  console.log('Press CTRL-C to stop\n');
 });
 
 module.exports = app;
